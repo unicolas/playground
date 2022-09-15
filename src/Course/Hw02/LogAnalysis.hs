a3cea4e80741123db895ffe1369c55341468a770
Nicolás Urquiola <nicourquiola@gmail.com>
2022-09-14 23:16:50 -0300

    Complete cis194 homework2

--- src/Course/Hw02/LogAnalysis.hs
+++ src/Course/Hw02/LogAnalysis.hs
@@ -1,6 +1,6 @@
-module Course.Hw02.LogAnalysis (parseMessage, parse) where
+module Course.Hw02.LogAnalysis (parseMessage, parse, whatWentWrong) where
 
-import Course.Hw02.Log (LogMessage (..), MessageType (..))
+import Course.Hw02.Log (LogMessage (..), MessageType (..), MessageTree (Leaf, Node))
 
 parseMessage :: String -> LogMessage
 parseMessage s = case words s of
@@ -12,3 +12,40 @@ parseMessage s = case words s of
 parse :: String -> [LogMessage]
 parse s = map parseMessage (lines s)
 
+timestamp :: LogMessage -> Int
+timestamp lm = case lm of
+  LogMessage Info ts _ -> ts
+  LogMessage Warning ts _ -> ts
+  LogMessage (Error _) ts _ -> ts
+  Unknown _ -> 0
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert lm t = case lm of
+  Unknown _ -> t
+  _ -> case t of
+    Leaf -> Node Leaf lm Leaf
+    Node l lm' r -> if timestamp lm < timestamp lm'
+      then Node (insert lm l) lm' r
+      else Node l lm' (insert lm r)
+
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder t = case t of
+  Leaf -> []
+  Node l lm r -> inOrder l ++ [lm] ++ inOrder r
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong lst = map message (inOrder (build (filter severity50 lst)))
+  where 
+    severity50 lm = case lm of
+      LogMessage (Error e) _ _ -> e >= 50
+      _ -> False
+
+message :: LogMessage -> String
+message m = case m of
+  LogMessage Info _ s -> s
+  LogMessage Warning _ s -> s
+  LogMessage (Error _) _ s -> s
+  Unknown s -> s
