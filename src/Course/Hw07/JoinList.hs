d6fd615422182f43544cbe1e57264c7f16a9d8fe
Nicolás Urquiola <nicourquiola@gmail.com>
2022-09-24 23:41:12 -0300

    Update cis194 homework7

--- src/Course/Hw07/JoinList.hs
+++ src/Course/Hw07/JoinList.hs
@@ -33,16 +33,39 @@ indexJ x (Append _ l r) =
   then indexJ x l
   else indexJ (x `div` 2) r
 
+sizeOf :: (Sized a1, Monoid a1) => JoinList a1 a2 -> Int
+sizeOf l = getSize $ size $ tag l
+
 indexJ' :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
-indexJ' s lst = case (s, lst) of
-  (_, Empty) -> Nothing
-  (x, _) | x < 0 -> Nothing
-  (x, jl) | x > sizeOf jl -> Nothing
-  (_, Single _ a) -> Just a
-  (x, Append _ l _) | x < sizeOf l -> indexJ x l
-  (x, Append _ _ r) -> indexJ (x `div` 2) r
-  where
-    sizeOf jl = getSize $ size $ tag jl
+indexJ' n jl = case jl of
+  Empty -> Nothing
+  _ | n < 0 -> Nothing
+  _ | n > sizeOf jl -> Nothing
+  Single _ a -> Just a
+  Append _ l r
+    | n < sizeOf l -> indexJ n l
+    | otherwise -> indexJ (n `div` 2) r
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ n jl = case jl of
+  Empty -> jl
+  _ | n < 0 -> jl
+  _ | n > sizeOf jl -> Empty
+  Single _ _ -> jl
+  Append _ l r
+    | n < sizeOf l -> dropJ n l +++ r
+    | otherwise -> dropJ (n `div` 2) r
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ n jl = case jl of
+  Empty -> jl
+  _ | n <= 0 -> Empty
+  _ | n >= sizeOf jl -> jl
+  Single _ _ -> jl
+  Append _ l r
+    | n <= sizeOf l -> takeJ n l
+    | otherwise -> l +++ takeJ (n `div` 2) r
+
 
 {- for testing -}
 
@@ -65,6 +88,14 @@ True
 
 >>> jl = Append (Size 4) (Append (Size 3) (Single (Size 1) 'y') (Append (Size 2) (Single (Size 1) 'e') (Single (Size 1) 'a'))) (Single (Size 1) 'h')
 >>> [indexJ (-1) jl, indexJ 0 jl, indexJ 1 jl, indexJ 2 jl, indexJ 3 jl, indexJ 4 jl] == [Nothing, Just 'y', Just 'e', Just 'a', Just 'h', Nothing]
+True
+
+>>> jl = Append (Size 4) (Append (Size 3) (Single (Size 1) 'y') (Append (Size 2) (Single (Size 1) 'e') (Single (Size 1) 'a'))) (Single (Size 1) 'h')
+>>> jlToList (dropJ 3 jl) == drop 3 (jlToList jl)
+True
+
+>>> jl = Append (Size 4) (Append (Size 3) (Single (Size 1) 'y') (Append (Size 2) (Single (Size 1) 'e') (Single (Size 1) 'a'))) (Single (Size 1) 'h')
+>>> jlToList (takeJ 2 jl) == take 2 (jlToList jl)
 True
 -}
 
