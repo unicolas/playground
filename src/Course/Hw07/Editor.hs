e8c3ff5decb47df2402a269b651f53db4afac427
Nicolás Urquiola <nicourquiola@gmail.com>
2022-09-26 21:03:32 -0300

    Add Applicative instance for Editor

--- src/Course/Hw07/Editor.hs
+++ src/Course/Hw07/Editor.hs
@@ -37,6 +37,10 @@ commands = map show [View, Edit, Next, Prev, Quit]
 newtype Editor b a = Editor (StateT (b,Int) IO a)
   deriving (Functor, Monad, MonadIO, MonadState (b,Int))
 
+instance Applicative (Editor b) where
+    pure = return
+    (<*>) = ap
+
 runEditor :: Buffer b => Editor b a -> b -> IO a
 runEditor (Editor e) b = evalStateT e (b,0)
 
